99cad7682940b5c466fe3fd22e37dcf579020dd5
Jonathan Chaney <jonchaney@gmail.com>
2018-01-14 12:51:25 -0800

    algos in golang

---
+++ algos/palindrome_permutation.go
@@ -0,0 +1,14 @@
+// palindrome permutation: Given a string, write a function to check if it us a permutation
+// of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards.
+// A permutation is a rearrangement of letters. The palindrome does not need to be limited to
+// just dictionary words
+
+package main
+
+func main() {
+	palindromePermutation("Taco Coa")
+}
+
+func palindromePermutation(str string) bool {
+	return false
+}
