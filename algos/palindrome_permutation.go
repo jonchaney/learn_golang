eb06597c2b5749dea5bd50b5a199d1f78e25b863
Jonathan Chaney <jonchaney@gmail.com>
2018-01-15 14:59:59 -0800

    palindrome permutations in go--O(n)

--- algos/palindrome_permutation.go
+++ algos/palindrome_permutation.go
@@ -5,10 +5,44 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	palindromePermutation("Taco Coa")
+	if palindromePermutation("Tact Coa") {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+
 }
 
+// create a hash counter - letters as keys and number it appears as values
+// if more than one letter appears an odd number of times, no palindrome exists
+// O(n)
 func palindromePermutation(str string) bool {
-	return false
+	// create hash table
+	// O(n)
+	letters := make(map[string]int)
+	for _, r := range str {
+		if string(r) == " " {
+			continue
+		}
+		letters[strings.ToLower(string(r))]++
+	}
+	// check if there is more than one letter that appears an odd
+	// number of times
+	// O(m) -- length of hash counter
+	odd := 0
+	for _, r := range str {
+		if letters[strings.ToLower(string(r))]%2 != 0 {
+			odd++
+		}
+		if odd > 1 {
+			return false
+		}
+	}
+	return true
 }
