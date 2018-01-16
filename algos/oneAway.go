86900368b7816f5e49f102367f6e24577415cfec
Jonathan Chaney <jonchaney@gmail.com>
2018-01-15 21:09:26 -0800

    main redeclared in same block

---
+++ algos/oneAway.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(oneAway("pale", "ple"))
+}
+
+func oneAway(str1, str2 string) bool {
+
+	return true
+}
--- learngo.go
+++ learngo.go
@@ -18,14 +18,15 @@ import (
 
 // A function definition. Main is special. It is the entry point for the
 // executable program. Love it or hate it, Go uses brace brackets.
-func main() {
-	// Println outputs a line to stdout.
-	// Qualify it with the package name, fmt.
-	fmt.Println("Hello world!")
 
-	// Call another function within this package.
-	beyondHello()
-}
+// func main() {
+// 	// Println outputs a line to stdout.
+// 	// Qualify it with the package name, fmt.
+// 	fmt.Println("Hello world!")
+
+// 	// Call another function within this package.
+// 	beyondHello()
+// }
 
 // Functions have parameters in parentheses.
 // If there are no parameters, empty parentheses are still required.
